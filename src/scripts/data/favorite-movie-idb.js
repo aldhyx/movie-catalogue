3f9d57164c3f9566132ef29791471554db112166
frialdhy ketty <aldyzkobf@gmail.com>
2021-07-05 20:40:50 +0800

    test: add integration testing for like module

--- src/scripts/data/favorite-movie-idb.js
+++ src/scripts/data/favorite-movie-idb.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-prototype-builtins */
 import { openDB } from 'idb';
 import CONFIG from '../globals/config';
 
@@ -11,12 +13,15 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
 
 const FavoriteMovieIdb = {
   async getMovie(id) {
+    if (!id) return;
     return (await dbPromise).get(OBJECT_STORE_NAME, id);
   },
   async getAllMovies() {
     return (await dbPromise).getAll(OBJECT_STORE_NAME);
   },
   async putMovie(movie) {
+    if (!movie.hasOwnProperty('id')) return;
+
     return (await dbPromise).put(OBJECT_STORE_NAME, movie);
   },
   async deleteMovie(id) {
